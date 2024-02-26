ca8a47e5c9d3df95a54b35cea1326db36585a548
yerbolat <erbolatt@live.com>
2024-02-26 19:47:30 +0600

    Initial commit

---
+++ src/main/java/dev/yerokha/lorby/LorbyApplication.java
@@ -0,0 +1,13 @@
+package dev.yerokha.lorby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LorbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LorbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yerokha/lorby/LorbyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yerokha.lorby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LorbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
